8453eed2c379a0fa0f44c9e734f52576627ed282
Bairi Sahitya <bairisahitya7@gmail.com>
2021-05-24 16:53:01 +0800

    added stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IqUJcHgdzSFd56pjFCoj6GV5gUfh99Q46BO3D50XaYgVRJbrfnkRZuaowg56iijZYzsRqyJ0UWwIwQ82IJ1S4y9000RF8wWJD";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
