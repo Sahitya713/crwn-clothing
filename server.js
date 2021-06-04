083b9172c8607ff9462451d230cab28710be6743
Bairi Sahitya <bairisahitya7@gmail.com>
2021-06-04 15:14:10 +0800

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const enforce = require("express-sslify");
+const cors = require("cors");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
