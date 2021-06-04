b2a1d5c6cc6fdc1d289fe8bdad4f54d71cddd451
Bairi Sahitya <bairisahitya7@gmail.com>
2021-06-04 15:22:05 +0800

    removed cors

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const enforce = require("express-sslify");
-const cors = require("cors");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -13,7 +12,6 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
